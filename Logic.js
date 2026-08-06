981d17f03dcce89efac5495005ff47f0935426d3
WaelKendouli <kendouliwael@gmail.com>
2026-08-06 20:06:58 +0200

    Adding the UI objects to javascript

---
+++ Logic.js
@@ -0,0 +1,25 @@
+const InputsUI = {
+
+     fullName : document.getElementById('fullName'),
+ position : document.getElementById('position'),
+ email : document.getElementById('email'),
+ phone : document.getElementById('phone'),
+ linkedin : document.getElementById('linkedin'),
+ github : document.getElementById('github'),
+ photo : document.getElementById('photo')
+
+
+}
+
+
+const UIElements = {
+
+ profileImage : document.getElementById('profileImage'),
+ userName : document.getElementById('userName'),
+ userPosition : document.getElementById('userPosition'),
+ emailText : document.getElementById('emailText'),
+ phoneText : document.getElementById('phoneText'),
+ linkedinText : document.getElementById('linkedinText'),
+ githubText : document.getElementById('githubText')
+
+}
